fed1f070262929597bda3072aff50e7c2b2e0f38
Nicolas Cusan <nicolas-cusan@users.noreply.github.com>
2026-01-22 10:04:05 +0100

    ss6 compatibility

--- src/AlpineExtension.php
+++ src/AlpineExtension.php
@@ -1,17 +1,19 @@
 <?php
+
 namespace Arillo\Alpine;
 
-use SilverStripe\Admin\LeftAndMainExtension;
 use SilverStripe\Core\Extension;
 use SilverStripe\View\Requirements;
 
-// class CMSMainExtension extends LeftAndMainExtension
+/**
+ * Extension to inject Alpine.js into SilverStripe CMS
+ */
 class AlpineExtension extends Extension
 {
-    public function init()
+    public function onBeforeInit()
     {
         Requirements::javascript(
-            'arillo/silverstripe-cms-alpine: client/alpinejs@3.12.3_dist_cdn.min.js',
+            'arillo/silverstripe-cms-alpine:client/alpinejs@3.12.3_dist_cdn.min.js',
             ['defer' => true]
         );
     }
